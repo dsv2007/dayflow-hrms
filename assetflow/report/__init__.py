f319c8851393aeb8980f4f7ab57be6bc78c51a07
dsv2007 <santhivarshinidevan@gmail.com>
2026-08-22 09:52:57 +0530

    Untrack all files and folders unrelated to Dayflow HRMS

--- assetflow/report/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import assetflow_bi_report
--- datahub_sentinel/examples/fixed_dbt_model.sql
+++
@@ -1,7 +0,0 @@
-WITH raw_data AS (
-    SELECT * FROM {{ source('production', 'raw_transactions') }}
-)
-SELECT 
-    id,
-    total_amount * 0.9 AS discounted_amount
-FROM raw_data
